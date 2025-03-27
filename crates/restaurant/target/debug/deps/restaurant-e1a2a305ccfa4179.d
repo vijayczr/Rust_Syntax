7b1129b703bcd24eff90991d9a9c5dbc3840e204
Vijay <vjvijay130@gmail.com>
2025-03-27 23:54:50 +0530

    new file ffor collections

---
+++ crates/restaurant/target/debug/deps/restaurant-e1a2a305ccfa4179.d
@@ -0,0 +1,7 @@
+D:\VJ_Projects\Rust_proj\Rust_Syntax\crates\restaurant\target\debug\deps\librestaurant-e1a2a305ccfa4179.rmeta: src\lib.rs
+
+D:\VJ_Projects\Rust_proj\Rust_Syntax\crates\restaurant\target\debug\deps\librestaurant-e1a2a305ccfa4179.rlib: src\lib.rs
+
+D:\VJ_Projects\Rust_proj\Rust_Syntax\crates\restaurant\target\debug\deps\restaurant-e1a2a305ccfa4179.d: src\lib.rs
+
+src\lib.rs:
---
+++ crates/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+D:\VJ_Projects\Rust_proj\Rust_Syntax\crates\restaurant\target\debug\librestaurant.rlib: D:\VJ_Projects\Rust_proj\Rust_Syntax\crates\restaurant\src\lib.rs
